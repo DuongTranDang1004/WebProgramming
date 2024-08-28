ac8044c5c79146b7d1701fc4553cf7c944abbf98
duongtran2004 <S3979381@rmit.edu.vn>
2024-08-28 10:54:16 +0700

    try fetch some API

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const connectDB = require("./config/db"); // MongoDB connection file
 const dotenv = require("dotenv"); // Dotenv is used to load environment variables
 const cors = require("cors");
+const path = require("path"); // Import the path module
 
 // Import Swagger configuration
 const { swaggerUi, swaggerDocs } = require("./config/swaggerConfig");
@@ -44,16 +45,36 @@ app.use("/instructors", instructorRoutes);
 app.use("/learners", learnerRoutes);
 app.use("/platformAdmins", platformAdminRoutes);
 
+//Serve static html files from "views" directory
+
+app.use(express.static(path.join(__dirname, "views")));
+
 // Root route
 app.get("/", (req, res) => {
   res.send("Welcome to the IT Learning platform API!");
 });
+
+//Duong mofidication start
+
+app.get("/coursesQueryResult", (req, res) => {
+  //browse course would be a better name
+  res.sendFile(
+    path.join(__dirname, "views", "learner", "coursesQueryResult.html")
+  );
+});
+
+app.use(express.static(path.join(__dirname, "public"))); //serve all files form public directory
+
+//Duong modification end
 
 // Start the server, run at local first, then deploy on https://itlearning.ddns.net/ later on
 app.listen(port, host, async () => {
   // Print out PID for easy killing of the server
   console.log(`Server PID: ${process.pid}`);
   console.log("MongoDB_URI:", process.env.MONGODB_URI);
+  console.log(
+    path.join(__dirname, "views", "learner", "coursesQueryResult.html")
+  );
 
   // Connect to MongoDB
   // Must be done before the server starts
