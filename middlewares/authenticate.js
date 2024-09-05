b6b04ec128c42ece476349181d226e860ddd3909
CuongDinh08 <contact@cuongdinh.dev>
2024-09-05 07:35:05 +0000

    Fix authenticate middleware

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -5,33 +5,33 @@ const Learner = require("../models/learnerModel")
 async function authenticateMiddleware(req, res, next) {
     res.locals.authenticated = false;
     // Check if authenticate header exist
-    if (!req.headers.authentication) {
+    if (!req.headers.authorization) {
         return next();
     }
     // In case authenticate header exist
-    const token = req.headers.authentication;
+    const token = req.headers.authorization;
     // Search for token in instructor
-    const instructor = await Instructor.findOne({ token: token });
-    if (instructor) {
+    const instructor = await Instructor.find({ token: token });
+    if (instructor.length > 0) {
         res.locals.authenticated = true;
         res.locals.role = "instructor";
-        res.locals.instructor = instructor;
+        res.locals.instructor = instructor[0];
         return next();
     }
     // Search for token in platform admin
-    const admin = await PlatformAdmin.findOne({ token: token });
-    if (admin) {
+    const admin = await PlatformAdmin.find({ token: token });
+    if (admin > 0) {
         res.locals.authenticated = true;
         res.locals.role = "admin";
-        res.locals.admin = admin;
+        res.locals.admin = admin[0];
         return next();
     }
     //Search for token in leaner
-    const learner = await Learner.findOne({token: token});
-    if (learner) {
+    const learner = await Learner.find({token: token});
+    if (learner > 0) {
         res.locals.authenticated=true;
         res.locals.role = "learner";
-        res.locals.learner = learner;
+        res.locals.learner = learner[0];
         return next();
     }
     return next();
