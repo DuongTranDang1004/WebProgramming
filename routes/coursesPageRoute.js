67fcb9be7bda34b42e35abf55059e0918bd49c29
CuongDinh08 <contact@cuongdinh.dev>
2024-09-11 13:16:57 +0000

    Add courses view.

---
+++ routes/coursesPageRoute.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/detail/:courseID", (req, res) => {
+  res.render("courses/detail", {
+    title: "Course Detail",
+    pageScripts: [
+      "/js/courses/detail.js",
+    ],
+  });
+});
+
+router.get("/detailedPerformance", (req, res) => {
+  res.render("courses/detailedPerformance", {
+    title: "Course Detail Performance",
+    pageScripts: [
+      "/js/courses/detailedPerformance.js",
+    ],
+  });
+});
+
+router.get("/pay", (req, res) => {
+  res.render("courses/pay", {
+    title: "Course Payment",
+    pageScripts: [
+      "/js/courses/pay.js",
+    ],
+  });
+});
+
+module.exports = router;
